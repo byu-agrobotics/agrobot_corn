f55da55e77ced85a802927cb2c673eeeea3d1326
Nelson Durrant <snelsondurrant@gmail.com>
2025-05-08 13:42:24 -0600

    Simplified scripts

--- update.sh
+++ update.sh
@@ -4,5 +4,4 @@
 # Pull the latest Docker image and GitHub changes
 
 docker pull byuagrobotics1/agrobot_2.0:latest
-current_branch=$(git branch --show-current)
-git pull origin $current_branch # specify upstream for the robot's git setup
+git pull
