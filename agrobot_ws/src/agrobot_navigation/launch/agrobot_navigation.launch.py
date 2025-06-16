763eac8df2fd1701f3e76f862d65a71777264ee7
Nelson Durrant <snelsondurrant@gmail.com>
2025-06-16 17:39:54 -0600

    updates

--- agrobot_ws/src/agrobot_navigation/launch/agrobot_navigation.launch.py
+++ agrobot_ws/src/agrobot_navigation/launch/agrobot_navigation.launch.py
@@ -3,10 +3,10 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        # launch_ros.actions.Node(
-        #     package='agrobot_navigation',
-        #     executable='roboclaw_wrapper',
-        # ),
+        launch_ros.actions.Node(
+            package='agrobot_navigation',
+            executable='roboclaw_wrapper',
+        ),
         launch_ros.actions.Node(
             package='agrobot_navigation',
             executable='drive_controller',
@@ -17,11 +17,11 @@ def generate_launch_description():
         #     executable='test_drive_controller',
         #     output='screen'
         # ),
-        launch_ros.actions.Node(
-            package='agrobot_navigation',
-            executable='test_center',
-            output='screen'
-        ),
+        # launch_ros.actions.Node(
+        #     package='agrobot_navigation',
+        #     executable='test_center',
+        #     output='screen'
+        # ),
         # launch_ros.actions.Node(
         #     package='agrobot_navigation',
         #     executable='test_turn',
