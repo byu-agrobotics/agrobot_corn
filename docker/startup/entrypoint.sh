dc970b937214ba83ff616cdde67d20995f861ad4
egaskin111 <elijahgaskin@gmail.com>
2025-10-19 13:49:02 -0600

    stable

--- docker/startup/entrypoint.sh
+++ docker/startup/entrypoint.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-# Created by Nelson Durrant & Braden Meyers, Feb 2025
-#
-# Runs automatic commands on Docker container startup
-# - This script won't throw any errors, but it will crash immediately if any command fails
-# - You can view the output of this script by running 'docker logs agrobot'
-
-set -e
-
-# Any tasks that need to be run on startup should be added here
-
-exec /bin/bash
+# unused as of oct 2025 - make sure you run docker in interactive terminal mode and you wont need this for now
